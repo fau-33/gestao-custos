e59a212e259b6a94cc3de3fed13e32eaca03ec58
Flávio Félix <fau_felix30@hotmail.com>
2025-06-11 14:10:48 -0300

    first commit

---
+++ src/main/java/com/flaviodevjava/gestao_custos/controller/GestaoDespesaController.java
@@ -0,0 +1,11 @@
+package com.flaviodevjava.gestao_custos.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/gestao")
+public class GestaoDespesaController {
+  @PostMapping("/create")
+  public void create() {}
+
+}
---
+++ src/test/java/com/flaviodevjava/gestao_custos/GestaoDeCustosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaviodevjava.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeCustosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
